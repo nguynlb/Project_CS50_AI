73df1b4999897ec732da44962b9fa801a54681ce
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-08-25 15:00:41 +0700

    implement all util

--- Week0/TicTacToe/tictactoe.py
+++ Week0/TicTacToe/tictactoe.py
@@ -36,35 +36,61 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] != EMPTY:
+                possible_actions.add((i, j))
+
+    return possible_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    board[action[0]][action[1]] = player(board)
+    return board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    possible_win = {(0, 1, 2), 
+                    (3, 4, 5),
+                    (6, 7, 8), 
+                    (0, 3 ,6), 
+                    (1, 4 ,7), 
+                    (2, 5, 8), 
+                    (0, 4, 8), 
+                    (2, 4, 6)}
+    
+    check_line = lambda i, j, k: (board[i // 3][i % 3] == board[j // 3][j % 3] == board[k // 3][k % 3])  \
+                                 and board[i // 3][i % 3] is not None
+    for line in possible_win:
+        if check_line(line):
+            return board[line[0] // 3][line[0] % 3]
+    
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) or _count_none(board) == 0:
+        return True
+    return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X: return 1
+    elif winner(board) == O: return -1
+    return 0
 
 
 def minimax(board):
