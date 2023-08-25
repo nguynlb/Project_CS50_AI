916c2434bd14b6c61bec04201ad1555e941f0cb8
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-08-25 14:45:43 +0700

    Initialize tictactoe AI pro

---
+++ Week0/TicTacToe/tictactoe.py
@@ -0,0 +1,74 @@
+"""
+Tic Tac Toe Player
+"""
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def _count_none(board):
+    total_none = 0
+    for row in board:
+        for cell in row:
+            total_none += (1 if cell == EMPTY else 0)
+    return total_none
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    return O if _count_none(board) % 2 == 0 else X
+
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    raise NotImplementedError
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    raise NotImplementedError
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    raise NotImplementedError
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    raise NotImplementedError
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    raise NotImplementedError
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    raise NotImplementedError
